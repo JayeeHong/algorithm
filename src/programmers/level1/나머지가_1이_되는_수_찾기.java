dee3a7d80b8cdf636d69a17ae814fda6b2e1c8cf
JayeeHong <hjy48486@naver.com>
2022-01-07 18:23:04 +0900

    프로그래머스 나머지가 1이 되는 수 찾기

---
+++ src/programmers/level1/나머지가_1이_되는_수_찾기.java
@@ -0,0 +1,18 @@
+package programmers.level1;
+
+public class 나머지가_1이_되는_수_찾기 {
+    public static int solution(int n) {
+        int answer = 1;
+
+        while (n % answer != 1) {
+            answer++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(10)); //3
+        System.out.println(solution(12)); //11
+    }
+}
