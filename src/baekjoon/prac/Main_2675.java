7679a0877aeb573cee75ba43568c7275d2c64833
JayeeHong <hjy48486@naver.com>
2022-04-27 23:38:27 +0900

    220426

---
+++ src/baekjoon/prac/Main_2675.java
@@ -0,0 +1,24 @@
+package baekjoon.prac;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_2675 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for (int i = 0; i < T; i++) {
+            StringBuilder sb = new StringBuilder();
+            st = new StringTokenizer(br.readLine(), " ");
+            int R = Integer.parseInt(st.nextToken());
+            String S = st.nextToken();
+            for (int j = 0; j < S.length(); j++) {
+                for (int k = 0; k < R; k++) sb.append(S.charAt(j));
+            }
+            System.out.println(sb);
+        }
+    }
+}
